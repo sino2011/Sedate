df39931f0d35157fdf970f486731354ccf388551
sino2011 <yassinafify626@gmail.com>
2026-07-20 09:38:01 +0300

    feat: add vercel config and update live api url

--- src/landing.vue
+++ src/landing.vue
@@ -22,7 +22,7 @@ const handleFormSubmit = async () => {
   isSubmitting.value = true;
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/schedule", {
+    const response = await fetch("https://sedate.vercel.app/api/schedule", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
